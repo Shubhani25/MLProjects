3157333d9e31c4062da7fe84eb8d791875345192
Shubhani25 <shubhanimulay25@gmail.com>
2022-01-05 21:14:29 +0530

    Added files

---
+++ HouseSalesPrediction/HouseSales/request.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "http://localhost:3306/results"
+r = requests.post(url, json={
+            'rate':5,
+            'sales_in_first_month':200,
+            'sales_in_second_month':400
+})
+
+print(r.json())
